6788ff2808e2426df2929bf391da411a95e49569
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2020-05-01 23:40:37 +0100

    Updates

---
+++ precache-manifest.bd06d91705182f120be651dc75976af4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca86f804981777b9f1db2290492428af",
+    "url": "/Calculator-App/index.html"
+  },
+  {
+    "revision": "cec3f7ffcf672c6fc253",
+    "url": "/Calculator-App/static/css/main.19f3dfb3.chunk.css"
+  },
+  {
+    "revision": "4032310ff0ba4bc13a62",
+    "url": "/Calculator-App/static/js/2.1d271656.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Calculator-App/static/js/2.1d271656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec3f7ffcf672c6fc253",
+    "url": "/Calculator-App/static/js/main.a1d30ed1.chunk.js"
+  },
+  {
+    "revision": "af4ab7af6e4730ffdcae",
+    "url": "/Calculator-App/static/js/runtime-main.a8736b7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
